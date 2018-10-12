a7c9c89cc3c482496286918335bfecf07d8a0c4e
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-10-11 19:09:14 -0700

    Added loader.io token

--- server/index.js
+++ server/index.js
@@ -4,13 +4,18 @@ const proxy = require('express-http-proxy');
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const compression = require('compression');
+// const compression = require('compression');
 
 // const router = require('./routes.js');
 
+/********* Loader.io *********/
+app.get('/loaderio-1ee1f35b8d9ff2a3ed249fcbb2f0d31a', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../loaderio-1ee1f35b8d9ff2a3ed249fcbb2f0d31a.txt'));
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(compression());
+// app.use(compression());
 
 app.use('/:id', express.static('public'));
 app.get('/api/photos/:idOrName/restaurants', proxy('ec2-18-216-127-98.us-east-2.compute.amazonaws.com'));
