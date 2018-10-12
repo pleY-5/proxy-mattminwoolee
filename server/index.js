332c5991dee1d29bacfe9adc5e7108d74d178661
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-10-11 17:35:59 -0700

    Adjust server routes

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 app.use(compression());
 
 app.use('/:id', express.static('public'));
-
+app.get('/api/photos/:idOrName/restaurants', proxy('ec2-18-216-127-98.us-east-2.compute.amazonaws.com'));
 app.set('port', process.env.PORT || 8080);
 
-app.use('/api', router);
+// app.use('/api', router);
 
 app.listen(app.get('port'), () => {
   console.log(`app is listening to port ${app.get('port')}`);
